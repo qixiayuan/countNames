16323399d672b3b14e6046ac68126aee9feaa089
Qixia <yuanqixia@gmail.com>
2017-11-06 09:40:14 +0100

    add script for count

---
+++ count.sh
@@ -0,0 +1,6 @@
+array=( "Elizabeth" "Jane" "Mary" "Catherine" "Lydia" )
+for element in ${array[@]}
+do
+    num=$(cat PrideAndPrejudice.txt | tr " " "\n" | grep -c "$element")
+    echo "number of $element is $num" 
+done
